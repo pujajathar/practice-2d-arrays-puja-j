9d15af13714c7028abb8f688de9a307efa0b0cd0
pujajathar <pujajathar91@.com>
2026-04-25 13:09:13 -0500

    my 2D Array

---
+++ 2darrays.js
@@ -0,0 +1,10 @@
+let allScores = [[25,20,24], [33, 24,32], [24,31,35]];
+console.log(allScores);
+
+let theaterSeating = [["A1", "A2", "A3", "A4"], ["B1", "B2", "B3", "B4"], ["C1", "C2", "C3", "C4"]];
+console.log(theaterSeating);
+
+let contactsList = [["Name", "Email", "Phone"], ["Alice", "alicelastname@gnmail.com", 2345656667], ["Ava", "lastneameava@gmail.com", 2346532454]];
+console.log(contactsList);
+
+
